8c081c0a441be1f692ed8a440751a01ef5815d33
Krzysztof Mataj <krzysztof.mataj@gmail.com>
2022-12-18 09:31:30 +0100

    work

--- src/Day18.hs
+++ src/Day18.hs
@@ -1,8 +1,15 @@
 module Day18 ( solution ) where
 
-import Common (Solution(Solution), NoSolution(..), readNum)
+import Common (Solution(Solution), NoSolution(..), readNum, toTriple)
+import Data.List.Split (splitOn)
+import Debug.Trace (traceShow)
 
 solution = Solution "day18" "" run
 
-run _ = (NoSolution, NoSolution)
+run input = let
+    cubes = parse input
+    in traceShow cubes (NoSolution, NoSolution)
 
+parse = map parseLine lines
+
+parseLine = toTriple . map readNum . splitOn "\n"
