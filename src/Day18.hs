5730d896e9131313dd45d9b0c051e0845615a60c
Krzysztof Mataj <krzysztof.mataj@gmail.com>
2022-12-18 21:04:42 +0100

    day18

--- src/Day18.hs
+++ src/Day18.hs
@@ -9,15 +9,27 @@ solution = Solution "day18" "" run
 
 run input = let
     cubes = parse input
-    in traceShow cubes (NoSolution, NoSolution)
+    in (part1 cubes, NoSolution)
 
 parse = map parseLine . lines
 
 parseLine = toTriple . map readNum . splitOn ","
 
 type Cube = (Int, Int, Int)
+type Scan = Set.Set Cube
 
 part1 :: [Cube] -> Int
 part1 cubes = let
     cubesSet = Set.fromList cubes
-    in 0
+    in sum $ map (visibleSides cubesSet) cubes
+
+neighbours :: Cube -> [Cube]
+neighbours (x, y, z) = [ (x+1, y, z)
+                       , (x-1, y, z)
+                       , (x, y+1, z)
+                       , (x, y-1, z)
+                       , (x, y, z+1)
+                       , (x, y, z-1) ]
+
+visibleSides :: Scan -> Cube -> Int
+visibleSides scan cube = length $ filter id $ map (`Set.notMember` scan) (neighbours cube)
