fc55751b8bfff5866f8fd11a72fe6597568ef81c
Krzysztof Mataj <krzysztof.mataj@gmail.com>
2023-01-16 19:42:48 +0100

    day22 part 2 progress

--- src/Day22.hs
+++ src/Day22.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TupleSections #-}
 module Day22 ( solution ) where
 
 import Common (Solution(Solution), NoSolution(..), readNum, parseArray, showArray, inArrayBounds, subArray)
@@ -7,7 +8,9 @@ import Data.Char (isNumber)
 import Debug.Trace (traceShow, trace)
 import Data.Array (Array, bounds, (!), elems)
 import Prelude hiding (Left, Right)
-import Data.List (nub, sort)
+import Data.List (nub, sort, find, unfoldr)
+import qualified Data.Map.Strict as Map
+import Data.Maybe (fromJust)
 
 {-
 The idea is to split input map into 6 square maps. Then do mapping for each map/edge.
@@ -86,17 +89,25 @@ solution = Solution "day22" "" run
 
 run input = let
     (forceField, path) = parseInput input
-    in (part1 forceField path, part2 forceField path)
+    forceField' = toSquareFields forceField
+    in (part1 forceField' path, part2 forceField' path)
 
 type ForceField = Array (Int, Int) Field
 type Path = [Instruction]
 data Instruction = Move Int | Turn Rotation  deriving (Show)
 data Rotation = Clockwise | CounterClockwise  deriving (Show)
 data Field = None | Empty | Wall  deriving (Show, Eq)
+type Point = (Int, Int)
+type SquareOffset = Point
 
 data SquareField = SquareField {
-    squareFieldOffset :: Point,
+    squareFieldOffset :: SquareOffset,
     squareFieldForceField :: ForceField }
+
+data ForceField' = ForceField' {
+    forceFieldSquareSize :: Int,
+    forceFieldSquares :: [SquareField]
+}
 
 parseInput :: String -> (ForceField, Path)
 parseInput input = let
@@ -117,18 +128,15 @@ parsePath str@(x:xs) = case x of
     _   -> Move (read numStr) : parsePath xs where
         (numStr, xs) = span isNumber str
 
-toSquareFields :: ForceField -> [SquareField]
+toSquareFields :: ForceField -> ForceField'
 toSquareFields forceField = let
     ((minY, minX), (maxY, maxX)) = bounds forceField
     edgeCoords = sort $ nub $ [x | y <- [minY..maxY-1], x <- [minX..maxX-1], isEdge forceField (y, x) (y, x+1)]
     squareSize = head $ zipWith (-) (tail edgeCoords) edgeCoords
     squaresPositions = [(y, x) | y <- [minY, minY+squareSize .. maxY], x <- [minX, minX + squareSize .. maxX]]
-    squares = [SquareField (y, x) $ subArray forceField ((y, x), (y + squareSize - 1, x + squareSize - 1)) | (y, x) <- squaresPositions]
-    nonEmptySquares = filter ((/=None) . head . elems . squareFieldForceField) squares
-    in traceShow (map squareFieldOffset nonEmptySquares) nonEmptySquares
-    -- find horizontal and vertical coordinates to split
-    -- divide whole space to SquareFields
-    -- filter out the ones that contains Empty only
+    squares = [SquareField (y `div` squareSize, x `div` squareSize) $ subArray forceField ((y, x), (y + squareSize - 1, x + squareSize - 1)) | (y, x) <- squaresPositions]
+    validSquares = filter ((/=None) . head . elems . squareFieldForceField) squares
+    in ForceField' squareSize validSquares
 
 isEdge :: ForceField -> Point -> Point -> Bool
 isEdge forceField a b = case (forceField ! a, forceField ! b) of
@@ -137,39 +145,95 @@ isEdge forceField a b = case (forceField ! a, forceField ! b) of
     (None, _)    -> True
     _            -> False
 
-part1 :: ForceField -> Path -> Int
-part1 forceField path = 1000 * (y+1) + 4 * (x+1) + fromEnum facing where
-    ((y, x), facing) = last $ goPath forceField path
+getSquareField :: ForceField' -> Point -> Maybe SquareField
+getSquareField squareFields point = find ((==point) . squareFieldOffset) (forceFieldSquares squareFields)
 
-part2 :: ForceField -> Path -> Int
+getForceField :: ForceField' -> Point -> ForceField
+getForceField squareFields = squareFieldForceField . fromJust . getSquareField squareFields
+
+getField :: ForceField' -> Position -> Field
+getField squareFields (offset, pos) = getForceField squareFields offset ! pos
+
+type Position = (SquareOffset, Point)
+data Facing = Right | Down | Left | Up  deriving (Show, Enum, Eq, Ord)
+type Mapping = Map.Map (SquareOffset, Facing) (SquareOffset, Facing)
+
+part1 :: ForceField' -> Path -> Int
+part1 forceField path =  let
+    mapping = simpleMapping forceField
+    squareSize = forceFieldSquareSize forceField
+    (((offsetY, offsetX), (y, x)), facing) = last $ goPath forceField mapping path
+    y' = offsetY * squareSize + y + 1
+    x' = offsetX * squareSize + x + 1
+    in 1000 * y' + 4 * x' + fromEnum facing
+
+part2 :: ForceField' -> Path -> Int
 part2 forceField path = let
-    squareFields = toSquareFields forceField
-    in length squareFields
+    in 0
 
-type Point = (Int, Int)
-data Facing = Right | Down | Left | Up  deriving (Show, Enum)
+simpleMapping :: ForceField' -> Mapping
+simpleMapping forceField = Map.fromList [((point, facing), simpleNeighbour forceField point facing)
+                                          | point <- map squareFieldOffset (forceFieldSquares forceField)
+                                          , facing <- [Left, Up, Right, Down]]
 
-goPath :: ForceField -> Path -> [(Point, Facing)]
-goPath forceField = scanl (move forceField) ((0, 0), Right)
+simpleNeighbour :: ForceField' -> SquareOffset -> Facing -> (SquareOffset, Facing)
+simpleNeighbour forceField point facing = let
+    facing' = oppositeFacing facing
+    nextFields = neighboursInDirection forceField facing point
+    prevFields = point : neighboursInDirection forceField facing' point
+    in case nextFields of
+        nextField:_ -> (nextField, facing')
+        _           -> (last prevFields, facing')
 
-move :: ForceField -> (Point, Facing) -> Instruction -> ((Int, Int), Facing)
-move _ (pos, facing) (Turn rotation) = (pos, rotate rotation facing)
-move forceField (pos, facing) (Move n) = (lastPos, facing) where
-    nextPositions = iterate (advance forceField facing) pos
-    positionsTypes = map (forceField !) nextPositions
+neighboursInDirection :: ForceField' -> Facing -> SquareOffset -> [Point]
+neighboursInDirection forceField facing = unfoldr (\p -> do
+                                                            neighbour <- maybeNeighbour forceField facing p
+                                                            return (neighbour, neighbour))
+    
+maybeNeighbour :: ForceField' -> Facing -> SquareOffset -> Maybe SquareOffset
+maybeNeighbour forceField facing offset = do
+    squareField <- getSquareField forceField (nextPoint offset facing)
+    return $ squareFieldOffset squareField
+
+goPath :: ForceField' -> Mapping -> Path -> [(Position, Facing)]
+goPath forceField mapping = scanl (move forceField mapping) ((squareFieldOffset $ head $ forceFieldSquares forceField, (0, 0)), Right)
+
+move :: ForceField' -> Mapping -> (Position, Facing) -> Instruction -> (Position, Facing)
+move _ _ (pos, facing) (Turn rotation) = (pos, rotate rotation facing)
+move forceField mapping (pos, facing) (Move n) = lastPos where
+    nextPositions = iterate (advance forceField mapping) (pos, facing)
+    positionsTypes = map (getField forceField . fst) nextPositions
     lastPos = fst $ last $ takeWhile ((/= Wall) . snd) $ take (n+1) $ filter ((/= None) . snd) (nextPositions `zip` positionsTypes)
 
-advance :: ForceField -> Facing -> Point -> Point
-advance forceField facing (y, x) = (y' `mod` (maxY+1), x' `mod` (maxX+1)) where
-    (_, (maxY, maxX)) = bounds forceField
-    (y', x') = case facing of
+advance :: ForceField' -> Mapping -> (Position, Facing) -> (Position, Facing)
+advance forceField mapping ((offset, point), facing) = let
+    squareForceField = getForceField forceField offset
+    point' = nextPoint point facing
+    in if inArrayBounds squareForceField point'
+        then ((offset, point'), facing)
+        else ((offset', rotatePoint (forceFieldSquareSize forceField - 1) facing facing' point), oppositeFacing facing') where
+            (offset', facing') = mapping Map.! (offset, facing)
+
+nextPoint :: Point -> Facing -> Point
+nextPoint (y, x) facing = case facing of
         Left  -> (y, x-1)
         Up    -> (y-1, x)
         Right -> (y, x+1)
         Down  -> (y+1, x)
+
+oppositeFacing :: Facing -> Facing
+oppositeFacing = toEnum . (`mod` 4) . (+2) . fromEnum
 
 rotate :: Rotation -> Facing -> Facing
 rotate rotation facing = toEnum $ (fromEnum facing + dir) `mod` 4 where
     dir = case rotation of
         Clockwise        ->  1
         CounterClockwise -> -1
+
+rotatePoint :: Int -> Facing -> Facing -> Point -> Point
+rotatePoint edge facingA facingB (y, x) = case (facingA, facingB) of
+    (Left, Right) -> (y, edge)
+    (Right, Left) -> (y, 0)
+    (Up, Down)    -> (edge, x)
+    (Down, Up)    -> (0, x)
+    _             -> error $ "unexpected transition: " ++ show facingA ++ " -> " ++ show facingB
